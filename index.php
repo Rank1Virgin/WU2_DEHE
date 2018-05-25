c9723380cd3a7ee78f9c50fa4b417250f3edb943
Rank1Virgin <32635958+Rank1Virgin@users.noreply.github.com>
2018-05-25 13:41:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*I Pseudokod skall ni mellan start och slut-tag för php beskriva struktur för webbsidan i sig.
+Det här kommer bland annat ligga till grund för hur er nav kommer att se ut.*/
+
+/*
+Start
+profil
+posta inlägg
+gilla
+
+*/
+
+?>
